f018abbf2bd151914e7a39982888a05a9dd80956
priyankasuperbazaar <priyankasuperbazaar2025@gmail.com>
2026-02-19 17:38:57 +0530

    Final fix: settings updated

--- config/settings.py
+++ config/settings.py
@@ -73,12 +73,16 @@ TEMPLATES = [
     },
 ]
 
+import dj_database_url
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
+
 
 AUTHENTICATION_BACKENDS = [
     'store.utils.EmailOrUsernameModelBackend',
