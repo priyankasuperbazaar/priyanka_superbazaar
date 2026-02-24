3e47c1e4cdd3c1e6d48c82e45bcd0f8bc3243576
priyankasuperbazaar <priyankasuperbazaar2025@gmail.com>
2026-02-24 20:43:16 +0530

    custmise

--- config/settings.py
+++ config/settings.py
@@ -117,7 +117,7 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
-#STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
